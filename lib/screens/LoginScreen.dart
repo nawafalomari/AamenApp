d25e2ad8982d343be60771e1a8998898cd78f01b
Hadi Al Marzooq <hadialmarzouq@outlook.com>
2021-11-20 14:26:12 +0300

    Login Page UI updated

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -28,67 +28,37 @@ class _LoginScreenState extends State<LoginScreen> {
   String password = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Text('تسجيل الدخول'),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomLabel(text: 'البريد الالكتروني'),
-                SizedBox(
-                  height: 20,
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Color(0xfff2f3f7),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff2470c7),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: const Radius.circular(70),
+                    bottomRight: const Radius.circular(70),
+                  ),
                 ),
-                CustomTextField(
-                  onFinish: (text) {
-                    email = text;
-                  },
-                  placeHolder: "exmaple@exmaple.com",
-                  isPassword: false,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                CustomLabel(text: 'كلمة المرور'),
-                SizedBox(
-                  height: 20,
-                ),
-                CustomTextField(
-                  onFinish: (text) {
-                    password = text;
-                  },
-                  placeHolder: '********',
-                  isPassword: true,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                MaterialButton(
-                  color: Colors.red.shade200,
-                  onPressed: () {
-                    if (email != '' && password != '') {
-                      sighnin(email, password);
-                    }
-                  },
-                  child: Text('تسجيل دخول'),
-                ),
-                MaterialButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Register()),
-                    );
-                  },
-                  child: Text('ما عندك حساب؟ سجّل'),
-                )
-              ],
+              ),
             ),
-          ),
-        ));
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildLogo(),
+                _buildContainer(),
+                _buildSignUpBtn(),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   void sighnin(String email, String password) async {
@@ -146,5 +116,209 @@ class _LoginScreenState extends State<LoginScreen> {
         print('Wrong password provided for that user.');
       }
     }
+  }
+
+  Widget _buildLogo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 70),
+          child: Text(
+            ' تطبيق آمن',
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.height / 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEmailRow() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        validator: (String? value) {
+          if (value!.isEmpty) {
+            return 'Please enter an email id';
+          }
+          return null;
+        },
+        onChanged: (value) {
+          setState(() {
+            email = value;
+          });
+        },
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.email), labelText: 'أيميلك يا حلو'),
+      ),
+    );
+  }
+
+  Widget _buildPasswordRow() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        obscureText: true,
+        validator: (String? value) {
+          if (value!.isEmpty) {
+            return 'Please enter a password';
+          }
+          if (value.length < 6) {
+            return 'Password should contain more than 5 characters';
+          }
+          return null;
+        },
+        onChanged: (value) {
+          setState(() {
+            password = value;
+          });
+        },
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.lock),
+          labelText: 'باسوردك؟',
+        ),
+      ),
+    );
+  }
+
+ 
+
+  Widget _buildLoginButton() {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      primary: Colors.blue,
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 1.4 * (MediaQuery.of(context).size.height / 20),
+          width: 5 * (MediaQuery.of(context).size.width / 10),
+          margin: EdgeInsets.only(bottom: 20),
+          child: ElevatedButton(
+            style: style,
+            onPressed: () {
+              if (email != '' && password != '') {
+                sighnin(email, password);
+              }
+            },
+            child: Text(
+              "تسجيل دخول",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildOrRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(bottom: 20.0),
+          child: Text(
+            '- أو؟ -',
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildContainer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20.0),
+          ),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "سجل دخول",
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height / 30,
+                      ),
+                    ),
+                  ],
+                ),
+                _buildEmailRow(),
+                _buildPasswordRow(),
+                _buildLoginButton(),
+                _buildOrRow(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSignUpBtn() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 40.0),
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Register()),
+              );
+            },
+            child: RichText(
+              text: TextSpan(children: [
+                TextSpan(
+                  text: 'ماعندك حساب؟ :(  ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: MediaQuery.of(context).size.height / 40,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                TextSpan(
+                    text: 'سوي حساب وشدعوة',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: MediaQuery.of(context).size.height / 40,
+                      fontWeight: FontWeight.bold,
+                    ))
+              ]),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
